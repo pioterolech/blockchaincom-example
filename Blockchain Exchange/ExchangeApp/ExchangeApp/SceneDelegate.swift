c03990aa5853897ff8f946a360c2363e0c6e408f
Piotr Olechnowicz <piotr.olechnowicz@gnb.pl>
2021-12-06 17:35:42 +0100

    Refactor in main app to improve visibility and code structure, make rootViewController set programmatically

--- Blockchain Exchange/ExchangeApp/ExchangeApp/SceneDelegate.swift
+++ Blockchain Exchange/ExchangeApp/ExchangeApp/SceneDelegate.swift
@@ -13,21 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let symbols = ViewController()
+            let navigation = UINavigationController(rootViewController: symbols)
+            window.rootViewController = navigation
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
-
 }
